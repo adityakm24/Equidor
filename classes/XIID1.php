913ac45ab58bdac0c007a1ef1af27bbfce0a6b9c
Aditya Krishnan Mohan <adityakm24@icloud.com>
2020-08-10 14:58:22 +0530

    Bug fixes
    Bugs fixed

--- classes/XIID1.php
+++ classes/XIID1.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: logdin/XllD1.php");
+    header("location: logdin/XIID1.php");
     exit;
 }
  
@@ -31,7 +31,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST["password"]);
     }
-    
+  
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
@@ -64,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: ../XIID1.php");
+                            header("location: logdin/XIID1.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
